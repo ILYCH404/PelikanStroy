ed966af8719e6d8eebe8aa6a53bd315f7ac21c60
ILYCH404 <ilyachuk77@gmail.com>
2022-09-12 17:11:13 +0300

    Initial commit

---
+++ src/main/java/com/pelicanstroy/PelikanStroyApplication.java
@@ -0,0 +1,11 @@
+package com.pelicanstroy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelikanStroyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PelikanStroyApplication.class);
+    }
+}
