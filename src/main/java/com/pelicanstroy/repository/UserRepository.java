9e80dc5c91d91477ccfc77adf28328919293fafa
ILYCH404 <ilyachuk77@gmail.com>
2022-09-17 18:30:45 +0300

    add getByEmail to UserRepository

--- src/main/java/com/pelicanstroy/repository/UserRepository.java
+++ src/main/java/com/pelicanstroy/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.pelicanstroy.repository;
 import com.pelicanstroy.model.User;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Transactional(readOnly = true)
 public interface UserRepository extends BaseRepository<User>{
+
+    Optional<User> getByEmail(String email);
 }
