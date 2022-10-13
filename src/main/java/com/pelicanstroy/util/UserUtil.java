5cce1b995dfbebae7577e30bf386a5f69a858318
ILYCH404 <ilyachuk77@gmail.com>
2022-10-13 14:04:37 +0300

    add userUtils

---
+++ src/main/java/com/pelicanstroy/util/UserUtil.java
@@ -0,0 +1,34 @@
+package com.pelicanstroy.util;
+
+import com.pelicanstroy.model.Role;
+import com.pelicanstroy.model.User;
+import com.pelicanstroy.to.UserTo;
+import lombok.experimental.UtilityClass;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@UtilityClass
+public class UserUtil {
+
+    public static final PasswordEncoder PASSWORD_ENCODER = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+    public static User createNewFromTo(UserTo userTo) {
+        return new User(null, userTo.getFirstName(), userTo.getMiddleName(), userTo.getLastName(), userTo.getPhone(), userTo.getEmail().toLowerCase(), userTo.getPassword(), Role.USER);
+    }
+
+    public static User updateFromTo(User user, UserTo userTo) {
+        user.setFirstName(userTo.getFirstName());
+        user.setMiddleName(userTo.getMiddleName());
+        user.setLastName(userTo.getLastName());
+        user.setPhone(userTo.getPhone());
+        user.setEmail(userTo.getEmail().toLowerCase());
+        user.setPassword(userTo.getPassword());
+        return user;
+    }
+
+    public static User prepareToSave(User user) {
+        user.setPassword(PASSWORD_ENCODER.encode(user.getPassword()));
+        user.setEmail(user.getEmail().toLowerCase());
+        return user;
+    }
+}
