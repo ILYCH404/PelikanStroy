68381cb5c1a5a4e7148a01e43cd14cb2d4cb025e
Ilya Chukanov <ilyachuk77@gmail.com>
2022-10-20 21:20:10 +0300

    change name

--- src/main/java/com/pelicanstroy/web/user/AbstractUserController.java
+++ src/main/java/com/pelicanstroy/web/user/AbstractUserController.java
@@ -2,21 +2,25 @@ package com.pelicanstroy.web.user;
 
 import com.pelicanstroy.model.User;
 import com.pelicanstroy.repository.UserRepository;
+import com.pelicanstroy.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
 public abstract class AbstractUserController {
 
     @Autowired
-    protected UserRepository userRepository;
+    protected UserRepository repository;
 
     public ResponseEntity<User> get(int id) {
-        return ResponseEntity.of(userRepository.findById(id));
+        return ResponseEntity.of(repository.findById(id));
     }
 
     public void delete(int id) {
-        userRepository.deleteById(id);
+        repository.deleteById(id);
     }
 
+    protected User prepareAndSave(User user) {
+        return repository.save(UserUtil.prepareToSave(user));
+    }
 
 }
